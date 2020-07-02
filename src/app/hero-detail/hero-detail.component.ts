9de68c19090efc58be8ad2573c2c8acdfbfe5f0f
Kim Hyeoncheol <effectmix@gmail.com>
2020-07-02 23:51:46 +0900

    5. Add In-app Navigation - Routable HeroDetailComponent with updating Hero Service

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
 import { Hero } from "../hero";
+import { HeroService } from "../hero.service";
 
 @Component({
   selector: 'app-hero-detail',
@@ -10,9 +14,42 @@ export class HeroDetailComponent implements OnInit {
 
   @Input() hero: Hero;
 
-  constructor() { }
+  /**
+   * 
+   * @param activatedRoute holds information about the route to this instance of the HeroDetailComponent.
+   * This component is interested in the route's parameters extractedfrom the URL.
+   * The "id" parameter is the id of the hero to display
+   * @param location is an Angular service for interacting with the browser.
+   * we will use it later to navigate back to the view that navigated here.
+   * @param heroService gets hero data from the remote server and this component will use it to ge the hero-to-display
+   */
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private location:Location,
+    private heroService: HeroService
+  ) { }
 
   ngOnInit(): void {
+    this.getHero();
+  }
+
+  /**
+   * The route.snapshot is a static image of route information shortly after the component was created.
+   * 
+   * The paramMap is a dictionary of route parameter values extracted from the URL. The "id" key returns
+   * the id of the hero to fetch
+   * 
+   * Route parameters are always strings. The JavaScript (+) operator converts the string to a number, which is
+   * what a hero id should be.
+   */
+  getHero(): void {
+    const id = +this.activatedRoute.snapshot.paramMap.get('id');
+    this.heroService.getHero(id)
+      .subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
