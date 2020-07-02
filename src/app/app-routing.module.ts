c6dfed843f5d6787b22e685d04631468af0ba8ae
Kim Hyeoncheol <effectmix@gmail.com>
2020-07-02 22:29:12 +0900

    5. Add In-app Navigation - Navigating to hero details with Modifying code in dashboard, hero details and hero component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from "@angular/router";
 import { HeroesComponent } from "./heroes/heroes.component";
 import { DashboardComponent } from "./dashboard/dashboard.component";
+import { HeroDetailComponent } from "./hero-detail/hero-detail.component";
 
 /**
  * ng generate module app-routing --flat --module=app
@@ -24,6 +25,7 @@ const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'heroes', component: HeroesComponent },
   { path: 'dashboard', component: DashboardComponent},
+  { path: 'detail/:id', component: HeroDetailComponent },
 ]
 /**
  * path: a string that matches the URL in the browser address bar
