7a352a5dede30cf14c775082df06b506e00b4971
Kim Hyeoncheol <effectmix@gmail.com>
2020-07-04 23:57:58 +0900

    6. Get Data from a Server - Enamble HTTP services and Simulate a data server

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,16 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { HttpClientModule } from "@angular/common/http";
+
+/**
+ * This is for mimicing communication with a remote data server by using the In-memory Web API module.
+ * After installing the module, the app will make requests to and receive responses from the HttpClient
+ * without knowing that the In-memory Web API is intercepting those requests,
+ * applying them to an in-memory data store, and returning simulated responses.
+ */
+import { HttpClientInMemoryWebApiModule } from "angular-in-memory-web-api";
+import { InMemoryDataService } from "./in-memory-data.service";
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
@@ -21,6 +31,15 @@ import { DashboardComponent } from './dashboard/dashboard.component';
     BrowserModule,
     FormsModule,
     AppRoutingModule,
+    HttpClientModule,
+    /**
+     * The HttpClientInMemoryWebApiModule module intercepts HTTP requests
+     * and returns simulated server responses.
+     * Remove it when a real servcer is ready to receive requests.
+     */
+    HttpClientInMemoryWebApiModule.forRoot(
+      InMemoryDataService, { dataEncapsulation: false }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
